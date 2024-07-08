66cbed3788b46a7bea1590e84e1a5fb37071efe6
antoniojvila <n.antonio.vila@gmail.com>
2024-07-08 16:31:23 -0400

    cors

--- proyecto/backend/core/settings.py
+++ proyecto/backend/core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'apps.score',
     'apps.signals',
     'apps.user',
+    'corsheaders',
 ]
 
 
@@ -86,6 +87,33 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "http://localhost:6969",
+    "http://127.0.0.1:6969",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
 ]
 
 ROOT_URLCONF = 'core.urls'
